5984d33e0d807fd554b4ee983036ee43812fbdab
Denis Mory <denismr7@gmail.com>
2020-05-31 19:06:28 +0200

    fixed horizontal scrolling by changing about animations

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var tl2 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 const animation = Power2.easeInOut;
 
-tl.fromTo("#personal-photo", 1, {x: 200, opacity: 0}, {x: 0, opacity: 1, ease: animation });
-tl.fromTo("#about-text", 1, {x: -200, opacity: 0}, {x: 0, opacity: 1, ease: animation }, "=-1");
+tl.fromTo("#personal-photo", 1, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: animation });
+tl.fromTo("#about-text", 1, {y: -200, opacity: 0 }, {y: 0, opacity: 1, ease: animation }, "=-1");
 tl2.fromTo("#tec-items-container", 1, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: animation });
 
 const scene = new ScrollMagic.Scene({
